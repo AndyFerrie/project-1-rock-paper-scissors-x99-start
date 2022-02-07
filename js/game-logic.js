f8484c5210686f059b744d6b0242c8289425e0bf
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-02-07 00:56:17 +0000

    building setplayermoves function

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,32 +1,67 @@
 // All code should be written in this file.
-let playerOneMoveOneType = '';
-let playerOneMoveTwoType = '';
-let playerOneMoveThreeType = '';
+let playerOneMoveOneType, 
+    playerOneMoveTwoType, 
+    playerOneMoveThreeType, 
+    playerOneMoveOneValue, 
+    playerOneMoveTwoValue, 
+    playerOneMoveThreeValue, 
+    playerTwoMoveOneType, 
+    playerTwoMoveTwoType, 
+    playerTwoMoveThreeType, 
+    playerTwoMoveOneValue, 
+    playerTwoMoveTwoValue, 
+    playerTwoMoveThreeValue;
 
-let playerOneMoveOneValue = '';
-let playerOneMoveTwoValue = '';
-let playerOneMoveThreeValue = '';
-
-let playerTwoMoveOneType = '';
-let playerTwoMoveTwoType = '';
-let playerTwoMoveThreeType = '';
-
-let playerTwoMoveOneValue = '';
-let playerTwoMoveTwoValue = '';
-let playerTwoMoveThreeValue = '';
+// A function called setPlayerMoves, which will take a string representing a player (in the form of 'Player One' or 'Player Two'), three move types, and three move values, and set the correct global move variables.
 
 function setPlayerMoves (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+    if (!moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeType || !moveThreeValue) {
+        return;
+    }
 
+    if (!isValidMoveType(moveOneType) || !isValidMoveType(moveTwoType) || !isValidMoveType(moveThreeType)) {
+        return;
+    }
+
+    if (!isValidMoveValue(moveOneValue) || !isValidMoveValue(moveTwoValue) || !isValidMoveValue(moveThreeValue)) {
+        return;
+    }
+
+    if (moveOneValue + moveTwoValue + moveThreeValue > 99) {
+        return;
+    }
+
+    if (player === 'Player One') {
+        playerOneMoveOneType = moveOneType
+        playerOneMoveOneValue = moveOneValue
+        playerOneMoveTwoType = moveTwoType
+        playerOneMoveTwoValue = moveTwoValue
+        playerOneMoveThreeType = moveThreeType
+        playerOneMoveThreeValue = moveThreeValue
+    } else if (player === 'Player Two') {
+        playerTwoMoveOneType = moveOneType
+        playerTwoMoveOneValue = moveOneValue
+        playerTwoMoveTwoType = moveTwoType
+        playerTwoMoveTwoValue = moveTwoValue
+        playerTwoMoveThreeType = moveThreeType
+        playerTwoMoveThreeValue = moveThreeValue
+    }
 }
 
-function getRoundWinner () {
+// A function called getRoundWinner, which takes a round number (1, 2, or 3), compares both player’s move types and values for that round, and returns the appropriate winner ('Player One', 'Player Two', or 'Tie')
 
+function getRoundWinner (i) {
+    roundNumber = i
 }
+
+// A function called getGameWinner, which compares both player’s move types and values for the whole game and returns the appropriate winner ('Player One', 'Player Two', or 'Tie')
 
 function getGameWinner () {
 
 }
 
+// Bonus: A function called setComputerMoves, which chooses three random moves for player two. The move type for each move should be completely random, and the move values should be random but add up to 99.
+
 function setComputerMoves () {
-    
+
 }
